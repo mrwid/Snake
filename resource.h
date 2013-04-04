997fe7d7e089c36b157288029f1d51bf45cd75dd
Mr.Wid <mr_wid@163.com>
2013-04-04 14:14:26 +0800

    资源文件 ========
    wid

---
+++ resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by resources.rc
+//
+#define IDI_ICON_GAME                   101
+#define IDD_HELP_FORM                   101
+#define IDR_GAME_START                  102
+#define IDR_EATING                      103
+#define IDR_READY                       104
+#define IDR_GO                          105
+#define IDC_BTN_OK                      1000
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
