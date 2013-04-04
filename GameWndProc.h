d9e0f8f6fe6af3bab969c65b5d0d479264e9cc3f
Mr.Wid <mr_wid@163.com>
2013-04-04 14:16:10 +0800

    主窗口回调函数 ==============
    wid

---
+++ GameWndProc.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//////////////////////////////////////////////////////////////////////////
+
+#include <windows.h>
+
+#include "DealBeginMenuMsg.h"
+#include "PlayingProc.h"
+#include "MacroDefine.h"
+#include "resource.h"
+
+//////////////////////////////////////////////////////////////////////////
+
+LRESULT CALLBACK WndProc( HWND, UINT, WPARAM, LPARAM );
